9f2e1c8098197041504976d08b86832ef6799ffa
LeonidCaminschi <leocer.leonid@gmail.com>
2025-03-13 16:13:05 +0200

    Implemented padding function with maxLen

--- ls.java
+++ ls.java
@@ -1,28 +1,41 @@
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ls {
     public static void list(String path) {
         Path dir = Paths.get(path);
+        Map<Path, Long> filesSize = new HashMap<Path, Long>();
         try {
 			DirectoryStream<Path> files = Files.newDirectoryStream(dir);
 			for (Path file: files) {
-                Path fileName = file.getFileName();
-				System.out.println(file.getFileName() + padding() + size(file));
+                filesSize.put(file.getFileName(), size(file));
+				// System.out.println(file.getFileName() + padding() + size(file));
                 // System.out.println(file.getFileName());
             }
             files.close();
+
+            int maxLen = 0;
+            for (Map.Entry<Path, Long> entry : filesSize.entrySet()) {
+                int filenameSize = entry.getKey().toString().length();
+                if (filenameSize > maxLen) {
+                    maxLen = filenameSize;
+                }
+            }
+
+            System.out.println(maxLen);
 		} catch (Exception e) {
 			System.out.println("Error: " + e);
 		}
     }
 
-    private static String padding() {
-        return "";
-    }
-
-    private static String compress(long bytes) {
-        return "";
+    private static String padding(int maxLen) {
+        String padding = "";
+        for (int i = 0; i < maxLen; i++) {
+            padding += " ";
+        }
+        return padding;
     }
 
     private static long size(Path item) {
